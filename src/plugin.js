25a166c8411be42a3e99a44e8c7aa6e7396d7637
Boopathi Rajaa <me@boopathi.in>
2016-04-17 23:58:21 +0200

    Perf improvements
    + Make it a Single pass through all the visitors
    + Trim multiple attribute visitors to a single one
    + Remove usage of path.traverse as no state is being passed - Replace it
    with `Object.assign(...visitors)`

--- src/plugin.js
+++ src/plugin.js
@@ -4,75 +4,45 @@ import {hyphenToCamel, namespaceToCamel} from './camelize';
 export default function (babel) {
   const t = babel.types;
 
-  // converts
-  // <svg stroke-width="5" xmlns:xlink="asdf">
-  // to
-  // <svg strokeWidth="5" xmlnsXlink="asdf">
-  const camelizeVisitor = {
+  const attrVisitor = {
     JSXAttribute(path) {
       if (t.isJSXNamespacedName(path.node.name)) {
+        // converts
+        // <svg xmlns:xlink="asdf">
+        // to
+        // <svg xmlnsXlink="asdf">
         path.node.name = t.jSXIdentifier(
           namespaceToCamel(path.node.name.namespace.name, path.node.name.name.name)
         );
       } else if (t.isJSXIdentifier(path.node.name)) {
+        // converts
+        // <tag class="blah blah1"/>
+        // to
+        // <tag className="blah blah1"/>
+        if (path.node.name.name === 'class') {
+          path.node.name.name = "className";
+        }
+
+        // converts
+        // <tag style="text-align: center; width: 50px">
+        // to
+        // <tag style={{textAlign: 'center', width: '50px'}}>
+        if (path.node.name.name === 'style') {
+          let csso = cssToObj(path.node.value.value);
+          let properties = Object.keys(csso).map(prop => t.objectProperty(
+            t.identifier(hyphenToCamel(prop)),
+            t.stringLiteral(csso[prop])
+          ));
+          path.node.value = t.jSXExpressionContainer(
+            t.objectExpression(properties)
+          );
+        }
+
+        // converts
+        // <svg stroke-width="5">
+        // to
+        // <svg strokeWidth="5">
         path.node.name.name = hyphenToCamel(path.node.name.name);
-      }
-    }
-  };
-
-  // converts
-  // <tag class="blah blah1"/>
-  // to
-  // <tag className="blah blah1"/>
-  const classNameVisitor = {
-    JSXAttribute(path) {
-      if (t.isJSXIdentifier(path.node.name) && path.node.name.name === 'class') {
-        path.node.name.name = "className";
-      }
-    }
-  };
-
-  // converts
-  // <tag style="text-align: center; width: 50px">
-  // to
-  // <tag style={{textAlign: 'center', width: '50px'}}>
-  const styleAttrVisitor = {
-    JSXAttribute(path) {
-      if (t.isJSXIdentifier(path.node.name) && path.node.name.name === 'style') {
-        let csso = cssToObj(path.node.value.value);
-        let properties = Object.keys(csso).map(prop => t.objectProperty(
-          t.identifier(hyphenToCamel(prop)),
-          t.stringLiteral(csso[prop])
-        ));
-        path.node.value = t.jSXExpressionContainer(
-          t.objectExpression(properties)
-        );
-        return;
-      }
-    }
-  };
-
-  // converts
-  // <svg>
-  // to
-  // <svg {...this.props}>
-  // after passing through attributes visitors
-  const svgVisitor = {
-    JSXOpeningElement(path) {
-      path.traverse(classNameVisitor);
-      path.traverse(camelizeVisitor);
-      path.traverse(styleAttrVisitor);
-
-      if (path.node.name.name.toLowerCase() === 'svg') {
-        // add spread props
-        path.node.attributes.push(
-          t.jSXSpreadAttribute(
-            t.memberExpression(
-              t.thisExpression(),
-              t.identifier('props')
-            )
-          )
-        );
       }
     }
   };
@@ -106,7 +76,28 @@ export default function (babel) {
         []
       )
     );
-  }
+  };
+
+  // converts
+  // <svg>
+  // to
+  // <svg {...this.props}>
+  // after passing through attributes visitors
+  const svgVisitor = {
+    JSXOpeningElement(path) {
+      if (path.node.name.name.toLowerCase() === 'svg') {
+        // add spread props
+        path.node.attributes.push(
+          t.jSXSpreadAttribute(
+            t.memberExpression(
+              t.thisExpression(),
+              t.identifier('props')
+            )
+          )
+        );
+      }
+    }
+  };
 
   // converts
   // <svg/>
@@ -114,28 +105,16 @@ export default function (babel) {
   // import React from 'react';
   // export default class SVG extends React.Component { render() { <svg/> }}
   // after passing through other visitors
+  const svgExpressionVisitor = {
+    ExpressionStatement(path) {
+      if (!path.get('expression').isJSXElement()) return;
+      if (path.get('expression.openingElement.name').node.name !== 'svg') return;
+      path.replaceWith(getExport(path.get('expression').node));
+    }
+  }
+
   const programVisitor = {
     Program(path) {
-      if (path.node.body.length === 0) throw new Error('No Content in SVG file');
-      if (path.node.body.length > 1) throw new Error('There is more than one root Element. We don\'t support this yet.');
-
-      let node = path.node.body[0];
-      if (!t.isExpressionStatement(node)) return;
-      if (!t.isJSXElement(node.expression)) return;
-      if (!t.isJSXIdentifier(node.expression.openingElement.name)) return;
-      if (node.expression.openingElement.name.name !== 'svg') return;
-
-      path.traverse(svgVisitor);
-
-      // replace the entire node to
-      // export default class extends React {
-      //   render () {
-      //     SVG_NODE
-      //   }
-      // }
-
-      path.node.body[0] = getExport(node.expression);
-
       // add import react statement
       path.node.body.unshift(
         t.importDeclaration(
@@ -145,11 +124,10 @@ export default function (babel) {
           t.stringLiteral('react')
         )
       );
-
     }
   }
 
   return {
-    visitor: programVisitor
+    visitor: Object.assign({}, programVisitor, svgExpressionVisitor, svgVisitor, attrVisitor)
   };
 }
