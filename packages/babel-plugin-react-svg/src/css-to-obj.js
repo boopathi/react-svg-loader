5fcf954e59b624fce02378f133494a9872281ac9
antoniopangallo <toto851@live.it>
2019-02-13 02:42:19 +0000

    issue-#205 (#206)

--- packages/babel-plugin-react-svg/src/css-to-obj.js
+++ packages/babel-plugin-react-svg/src/css-to-obj.js
@@ -2,12 +2,14 @@
 
 export default function cssToObj(css: string) {
   let o = {};
-  let elements = css.split(";");
-  elements.filter(el => !!el).map(el => {
-    let s = el.split(":"),
-      key = s.shift().trim(),
-      value = s.join(":").trim();
-    o[key] = value;
-  });
+  if (typeof css !== "undefined") {
+    let elements = css.split(";");
+    elements.filter(el => !!el).map(el => {
+      let s = el.split(":"),
+        key = s.shift().trim(),
+        value = s.join(":").trim();
+      o[key] = value;
+    });
+  }
   return o;
 }
